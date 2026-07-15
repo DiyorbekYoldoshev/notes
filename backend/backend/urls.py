c89aded01ae11e04735ba4e826e422b11a2cd99f
DiyorbekYoldoshev <yoldoshevd979@gmail.com>
2026-07-15 22:42:20 +0500

    Initial commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from backend.api.views import CreateUserView
+from api.views import CreateUserView, ListUserView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/user/register/', CreateUserView.as_view(),name='register'),
-    path('api/token/', TokenObtainPairView.as_view(),name='get_token'),
-    path('api/token/refresh/', TokenRefreshView.as_view(),name='refresh'),
-    path('api-auth/', include('rest_framework.urls'))
+    path("admin/", admin.site.urls),
+    path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api/user/", ListUserView.as_view(), name="list"),
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("api-auth/", include("rest_framework.urls")),
+    path('api/',include('api.urls'))
 ]
